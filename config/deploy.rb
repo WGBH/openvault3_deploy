a5fd019a1adf3cc08fe5c2b602e49f91f7c95177
Andrew Myers <afredmyers@gmail.com>
2016-01-25 15:15:55 -0500

    Fix application name (cannot have spaces)

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for current version of Capistrano
 lock '3.3.5'
 
-set :application, 'Open Vault'
+set :application, 'openvault'
 set :repo_url, 'https://github.com/WGBH/openvault3.git'
 
 # Default branch is :master
