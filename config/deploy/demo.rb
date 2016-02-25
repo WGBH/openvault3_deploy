5d7631190c78ff847351b175969f907d46ad2d27
Andrew Myers <afredmyers@gmail.com>
2016-02-25 11:18:09 -0500

    Parameterizes host IP and ssh key for demo stage
    Uses enviornment variables OV_HOST, OV_SSH_KEY

---
+++ config/deploy/demo.rb
@@ -0,0 +1,12 @@
+raise ArgumentError, "Missing required environment variable, OV_HOST." unless ENV['OV_HOST']
+raise ArgumentError, "Missing required environment variable, OV_SSH_KEY." unless ENV['OV_SSH_KEY']
+
+server ENV['OV_HOST'],
+  user: 'ec2-user',
+  roles: %w{web app},
+  ssh_options: {
+    user: 'ec2-user',
+    keys: [ENV['OV_SSH_KEY']],
+    forward_agent: false,
+    auth_methods: %w(publickey)
+  }
