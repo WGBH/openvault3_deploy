18a6ee5de30ce44a60b0ae5e9d34ad705db08148
Andrew Myers <afredmyers@gmail.com>
2016-02-26 13:30:04 -0500

    Adds db to demo server role
    needed so that rake db:migrate gets run.

--- config/deploy/demo.rb
+++ config/deploy/demo.rb
@@ -3,7 +3,7 @@ raise ArgumentError, "Missing required environment variable, OV_SSH_KEY." unless
 
 server ENV['OV_HOST'],
   user: 'ec2-user',
-  roles: %w{web app},
+  roles: %w{web app db},
   ssh_options: {
     user: 'ec2-user',
     keys: [ENV['OV_SSH_KEY']],
