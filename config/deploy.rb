6a318835c8d5a4a308a210fb27466f886640154e
Andrew Myers <afredmyers@gmail.com>
2016-04-11 10:53:06 -0400

    Allows setting git branch with env var (#58)
    closes #44.

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,8 +4,13 @@ lock '3.3.5'
 set :application, 'openvault'
 set :repo_url, 'https://github.com/WGBH/openvault3.git'
 set :rails_env, 'production'
-# Ask for github branch
-ask :branch, 'master'
+
+if ENV['OV_BRANCH']
+  set :branch, ENV['OV_BRANCH']
+else
+  ask :branch, 'master'
+end
+
 set :linked_dirs, fetch(:linked_dirs, []).push('log')
 namespace :deploy do
   after :updated, :ensure_jetty_is_installed do
