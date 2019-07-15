bd591039b7f4d1a30736e6ec6b2d00d9731cd4e1
foglabs <foglaboratories@gmail.com>
2019-07-15 14:43:16 -0400

    'de-openvault-ize env var names'

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,29 @@
 # For more options, see http://capistranorb.com/documentation/getting-started/configuration/#
 # config valid only for current version of Capistrano
 lock '3.3.5'
+
+# Change These To Order
 set :application, 'openvault'
 set :repo_url, 'https://github.com/WGBH/openvault3.git'
+
+
+
 set :rails_env, 'production'
+
+
+if ENV['BRANCH'] && ENV['BRANCH'].length > 0
+  set :branch, ENV['BRANCH']
+else
+  ask :branch, 'master'
+end
+
+# Add the path to bundler, /home/ec2-user/bin, to $PATH env var.
+set :linked_dirs, fetch(:linked_dirs, []).push('tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'tmp/downloads', 'jetty')
+set :rails_env, :production
+set :default_env, { 'PATH' => '$PATH:/home/ec2-user/bin' }
+set :bundle_flags, '--deployment'
+set :keep_releases, 1
+
 
 # Require confirmation by user if the repo is in a dirty state.
 include GitHelper
--- config/deploy/aws.rb
+++ config/deploy/aws.rb
@@ -1,12 +1,12 @@
-raise ArgumentError, "Missing required environment variable, OV_HOST." unless ENV['OV_HOST']
-raise ArgumentError, "Missing required environment variable, OV_SSH_KEY." unless ENV['OV_SSH_KEY']
+raise ArgumentError, "Missing required environment variable, SSH_HOST." unless ENV['SSH_HOST']
+raise ArgumentError, "Missing required environment variable, SSH_KEY." unless ENV['SSH_KEY']
 
-server ENV['OV_HOST'],
+server ENV['SSH_HOST'],
   user: 'ec2-user',
   roles: %w{web app db},
   ssh_options: {
     user: 'ec2-user',
-    keys: [ENV['OV_SSH_KEY']],
+    keys: [ENV['SSH_KEY']],
     forward_agent: false,
     auth_methods: %w(publickey)
   }
