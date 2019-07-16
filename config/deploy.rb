ec48d3113d34683e4c8c1c8320d5d64876db6a57
foglabs <foglaboratories@gmail.com>
2019-07-16 10:54:54 -0400

    'combo platter'

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,12 +2,9 @@
 # config valid only for current version of Capistrano
 lock '3.3.5'
 
-# Change These To Order
-set :application, 'openvault'
-set :repo_url, 'https://github.com/WGBH/openvault3.git'
-
-
-
+# 'openvault' or 'AAPB'
+set :application, ENV['APP_NAME']
+set :repo_url, ENV['REPO_URL']
 set :rails_env, 'production'
 
 
@@ -17,13 +14,22 @@ else
   ask :branch, 'master'
 end
 
+# Default value for :log_level is :debug
+set :log_level, :info
+# added from AAPB, not sure why this wasn't necessary for openvault
+# Default value for :linked_files is []
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/ci.yml')
+set :passenger_environment_variables, {
+  path: '/usr/local/share/gems/gems/passenger-5.3.4/bin/passenger:$PATH',
+}
+set :passenger_restart_with_touch, true
+
 # Add the path to bundler, /home/ec2-user/bin, to $PATH env var.
 set :linked_dirs, fetch(:linked_dirs, []).push('tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'tmp/downloads', 'jetty')
 set :rails_env, :production
 set :default_env, { 'PATH' => '$PATH:/home/ec2-user/bin' }
 set :bundle_flags, '--deployment'
 set :keep_releases, 1
-
 
 # Require confirmation by user if the repo is in a dirty state.
 include GitHelper
@@ -33,5 +39,9 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log')
 namespace :deploy do
   after :updated, :ensure_jetty_is_installed do
     invoke 'jetty:install'
+  end
+
+  if ENV['APP_NAME'] == 'AAPB'
+    after :finishing, 'api:add_credentials_to_env_variables'
   end
 end
